cc1640f20d2ad573edac8567fbb3c7f6d0235e0b
fmozart <sucoinvejavel@gmail.com>
2024-08-21 20:33:51 -0300

    Add files via upload

---
+++ projeto/projeto/newsp/newsapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from newsapp.models import News, Comment, Category
+
+# Register your models here.
+class AdminNews(admin.ModelAdmin):
+    list_display=('title','category','add_time')
+admin.site.register(News,AdminNews)
+
+class AdminComment(admin.ModelAdmin):
+    list_display=('news','email','comment','status')
+admin.site.register(Comment,AdminComment) 
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('title', 'category_image')
+admin.site.register(Category, AdminCategory)
